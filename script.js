e12d9f48f42a22805ccbfbea19cef3bf65f08192
ankit chaudhary <ac329888@gmail.com>
2025-12-01 21:26:19 +0530

    most optimized version

--- script.js
+++ script.js
@@ -1,31 +1,38 @@
-// const red=document.getElementById('red')
-// const yellow=document.getElementById('yellow')
-// const pink=document.getElementById('pink')
-// const blue=document.getElementById('blue')
+// // const red=document.getElementById('red')
+// // const yellow=document.getElementById('yellow')
+// // const pink=document.getElementById('pink')
+// // const blue=document.getElementById('blue')
+
+// // const body=document.body;
+
+// // red.addEventListener('click',()=>{
+// //  body.style.backgroundColor='red';
+
+// // })
+// // yellow.addEventListener('click',()=>{
+// //  body.style.backgroundColor='yellow';
+
+// // })
+// // pink.addEventListener('click',()=>{
+// //  body.style.backgroundColor='pink';
+
+// // })
+// // blue.addEventListener('click',()=>{
+// //  body.style.backgroundColor='blue';
+
+// // })
+// const button=document.querySelectorAll('button')
 
 // const body=document.body;
-
-// red.addEventListener('click',()=>{
-//  body.style.backgroundColor='red';
-
+// button.forEach((butt)=>{
+//  butt.addEventListener('click',()=>{
+//   body.style.backgroundColor=butt.id;
+//  })
 // })
-// yellow.addEventListener('click',()=>{
-//  body.style.backgroundColor='yellow';
-
-// })
-// pink.addEventListener('click',()=>{
-//  body.style.backgroundColor='pink';
-
-// })
-// blue.addEventListener('click',()=>{
-//  body.style.backgroundColor='blue';
-
-// })
-const button=document.querySelectorAll('button')
 
 const body=document.body;
-button.forEach((butt)=>{
- butt.addEventListener('click',()=>{
-  body.style.backgroundColor=butt.id;
- })
+const root=document.getElementById('root')
+root.addEventListener('click',(event)=>{
+body.style.backgroundColor=event.target.id;
 })
+
